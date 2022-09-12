07fa4c9ae335e710e6d430ba7df2e347c11f31b9
Mati <mati.schneider@markforged.com>
2022-09-12 05:45:31 -0400

    cleared basic template for main file

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 
-def print_hi(name):
-
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
